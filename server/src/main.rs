a66f86bb6d66efe7775673b2b6b4081c2eaaafaa
표 영종 <pyjong1999@ajou.ac.kr>
2025-02-05 23:06:59 +0900

    keep going..

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::VecDeque;
 use rand::seq::SliceRandom;
+use std::io;
 
 struct Player {
     pub name: String,
-    chips: u32,`
+    chips: u32,
     state: PlayerState,
     pub hands: Option<(String, String)>,
     player_pot: u32,
@@ -20,11 +21,11 @@ enum PlayerState {
 }
 
 struct Game {
-    players: Vec<Player>,
-    deck: VecDeque<String>,
+    players: Vec<Player>,   
+    deck: VecDeque<String>, 
     pot: u32,
     board: Vec<String>,
-    dealer_idx: u32,
+    dealer_idx: usize,
     blind: u32,
 }
 
@@ -35,7 +36,7 @@ impl Player {
             chips,
             state: PlayerState::Waiting,
             hands: None,
-            b
+            player_pot: 0,
         }
     }
 
@@ -51,7 +52,44 @@ impl Player {
         }
     }
 
-    
+    pub fn alive(&self) -> bool {
+        match self.state {
+            PlayerState::Idle => true,
+            PlayerState::Check => true,
+            PlayerState::Call => true,
+            PlayerState::Raise => true,
+            PlayerState::Fold => false,
+            PlayerState::AllIn => true,
+            PlayerState::Waiting => false,
+        }
+    }
+
+    pub fn check(&mut self) {
+        self.state = PlayerState::Check;
+    }
+
+    pub fn call(&mut self, size: u32) {
+        self.chips -= size;
+        self.player_pot += size;
+        self.state = PlayerState::Call;
+    }
+
+    pub fn raise(&mut self, size: u32) {
+        self.chips -= size;
+        self.player_pot += size;
+        self.state = PlayerState::Raise;
+    }
+
+    pub fn fold(&mut self) {
+        self.state = PlayerState::Fold;
+    }
+
+    pub fn allin(&mut self) {
+        self.player_pot += self.chips;
+        self.chips = 0;
+        self.state = PlayerState::AllIn;
+    }
+
 }
 
 impl Game {
@@ -90,6 +128,7 @@ impl Game {
             chips,
             state: PlayerState::Waiting,
             hands: None,
+            player_pot: 0,
         };
 
         self.players.push(player);
@@ -101,7 +140,7 @@ impl Game {
         self.board = Vec::new();
         self.print_deck();
         self.pot = 0;
-        self.dealer_idx = (self.dealer_idx + 1) % len(self.players);
+        self.dealer_idx = (self.dealer_idx + 1) % self.players.len();
 
         // Free Flop
         for player in self.players.iter_mut() {
@@ -117,7 +156,7 @@ impl Game {
 
         self.blind_bet();
 
-        self.betting_phase();
+        self.betting_phase(false);
 
         // Flop
         for _ in 0..3 {
@@ -125,17 +164,17 @@ impl Game {
         }
 
         self.print_board();
-        self.betting_phase();
+        self.betting_phase(false);
 
         // Turn
         self.board.push(self.deck.pop_front().unwrap());
         self.print_board();
-        self.betting_phase();
+        self.betting_phase(false);
 
         // River
         self.board.push(self.deck.pop_front().unwrap());
         self.print_board();
-        self.betting_phase();
+        self.betting_phase(false);
 
         self.show_down();
 
@@ -176,34 +215,63 @@ impl Game {
 
     fn betting_phase(&mut self, is_free_flop: bool) {
 
-        let sb_idx = (self.dealer_idx + 1) % len(players);
-        let mut cur_player_idx;
-        let mut call_pot;
+        let sb_idx = (self.dealer_idx + 1) % self.players.len();
+        let mut cur_player_idx: usize;
+        let mut call_pot: u32;
 
         if is_free_flop {
 
-            let bb_idx = (sb_idx + 1) % len(players);
-            cur_player_idx = (sb_idx + 2) % len(players);
+            let bb_idx = (sb_idx + 1) % self.players.len();
+            cur_player_idx = (sb_idx + 2) % self.players.len();
 
-            players[sb].Raise(self.blind / 2);
-            players[bb].Raise(self.blind);
+            self.player_raise(sb_idx as usize, self.blind / 2);
+            self.player_raise(bb_idx as usize, self.blind);
 
             call_pot = self.blind;
             
-            self.pot = self.blind * 1.5;
+            self.pot = ( self.blind as f32 * 1.5 ) as u32;
             
         } else {
             cur_player_idx = sb_idx;
-            call_pot = find_largest_player_pot();
+            call_pot = self.find_largest_player_pot();
         }
 
-        while !is_bet_finished(players[cur_player_idx]) {
+        while !self.is_bet_finished(cur_player_idx as usize, &call_pot) {
+
+            let player = &self.players[cur_player_idx];
+
+            let mut action: String = String::new();
+            println!("{}, choose your action. 1: Check, 2: Call, 3: Raise, 4: Fold.", player.name);
+            println!("your chips amount is {}. and..", player.chips);
             
+            if call_pot == 0 {
+                println!("Now call pot is 0. You can check.");
+            } else {
+                println!("You have to bet {} to call...", call_pot - player.player_pot);
+            }
+
+            io::stdin().read_line(&mut action).expect("Read Error");
+            
+
+
+            cur_player_idx = (cur_player_idx + 1) & self.players.len();
         }
+
     }
 
-    fn is_bet_finished(player: Player, call_pot: &u32) -> bool {
-        if call_pot == player.player_pot && player.alive() {
+    fn player_raise(&mut self, idx: usize, size: u32) -> u32{
+
+        self.players[idx].raise(size);
+        self.pot += size;
+
+        self.players[idx].player_pot
+    }
+
+    fn is_bet_finished(&mut self, idx: usize, call_pot: &u32) -> bool {
+
+        let player = &self.players[idx];
+
+        if call_pot == &player.player_pot && player.alive() {
             true
         } else {
             false
@@ -214,11 +282,15 @@ impl Game {
 
     }
 
+    fn find_largest_player_pot(&self) -> u32 {
+        1
+    }
+
 }
 
 fn main() {
 
-    let mut game = Game::new();
+    let mut game = Game::new(10);
     game.insert_player("Steve".to_string(), 1000);
     game.insert_player("Peter".to_string(), 1000);
     game.insert_player("ByungHyeok".to_string(), 1000);
@@ -228,3 +300,11 @@ fn main() {
 
 }
 
+/* 다음 해야할 것들
+
+1. dealer_idx -> sb_idx로 바꿔도 될 듯
+2. is_bet_finished 함수에 예외 없는지 곰곰히 생각
+3. find_largest_player_pot 구현
+4. betting_phase -> while 문 마저 구현현
+
+끝 */
